abe2c7be2a7a2bb5fd85801f74a1308c9e35df28
i-bara <xulvjin@buaa.edu.cn>
2024-12-12 00:15:26 +0800

    clean up benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,6 @@
 import os
-import sys
 import re
 import json
-import datetime
 import argparse
 
 
@@ -19,70 +17,17 @@ def parse_args():
     return args
 
 
-args = parse_args()
-
-all_config = dict()
-all_config['methods'] = ['vanilla', 'drgcn', 'smote', 'imgagn', 'dpgnn', 'mixup', 'ens', 'tam', 'lte4g', 'sann', 'topoauc', 'sha', 'renode', 'pastel', 'hyperimba', 'igraphmix', 'bat']
-all_config['datasets'] = ['Cora_100', 'Cora_20', 'Cora_1', 
-                          'CiteSeer_100', 'CiteSeer_20', 'CiteSeer_1', 
-                          'PubMed_100', 'PubMed_20', 'PubMed_1', 
-                          'chameleon_100', 'chameleon_20', 'chameleon_1', 
-                          'squirrel_100', 'squirrel_20', 'squirrel_1', 
-                          'Actor_100', 'Actor_20', 'Actor_1', 
-                          'Wisconsin_100', 'Wisconsin_20', 'Wisconsin_1', 
-                          'Amazon-Photo_100', 'Amazon-Photo_20', 'Amazon-Photo_1', 
-                          'Amazon-Computers_100', 'Amazon-Computers_20', 'Amazon-Computers_1', 
-                          'ogbn-arxiv_100', 'ogbn-arxiv_20', 'ogbn-arxiv_1', 
-                          'Amazon-Photo', 'Amazon-Computers', 'Coauthor-CS', 'ogbn-arxiv']
-all_config['seeds'] = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-
-
-def config_args(method, dataset, seed):
-    method_config = '--method ' + method
-    if dataset.endswith('_100') or dataset.endswith('_20') or dataset.endswith('_1'):
-        dataset_name, imb_ratio = dataset.split('_')
-        dataset_config = f'--dataset {dataset_name} --imb_ratio {imb_ratio}'
-    # if dataset == 'Cora_100':
-    #     dataset_config = '--dataset Cora --imb_ratio 100'
-    # elif dataset == 'Cora_20':
-    #     dataset_config = '--dataset Cora --imb_ratio 20'
-    # elif dataset == 'CiteSeer_100':
-    #     dataset_config = '--dataset CiteSeer --imb_ratio 100'
-    # elif dataset == 'CiteSeer_20':
-    #     dataset_config = '--dataset CiteSeer --imb_ratio 20'
-    # elif dataset == 'PubMed_100':
-    #     dataset_config = '--dataset PubMed --imb_ratio 100'
-    # elif dataset == 'PubMed_20':
-    #     dataset_config = '--dataset PubMed --imb_ratio 20'
-    # elif dataset == 'chameleon_100':
-    #     dataset_config = '--dataset chameleon --imb_ratio 100'
-    # elif dataset == 'chameleon_20':
-    #     dataset_config = '--dataset chameleon --imb_ratio 20'
-    # elif dataset == 'squirrel_100':
-    #     dataset_config = '--dataset squirrel --imb_ratio 100'
-    # elif dataset == 'squirrel_20':
-    #     dataset_config = '--dataset squirrel --imb_ratio 20'
-    # elif dataset == 'Actor_100':
-    #     dataset_config = '--dataset Actor --imb_ratio 100'
-    # elif dataset == 'Actor_20':
-    #     dataset_config = '--dataset Actor --imb_ratio 20'
-    # elif dataset == 'Wisconsin_100':
-    #     dataset_config = '--dataset Wisconsin --imb_ratio 100'
-    # elif dataset == 'Wisconsin_20':
-    #     dataset_config = '--dataset Wisconsin --imb_ratio 20'
-    elif dataset in ['Amazon-Photo', 'Amazon-Computers', 'Coauthor-CS', 'ogbn-arxiv']:
-        dataset_config = '--dataset ' + dataset + ' --imb_ratio 0'  # Do not adjust the imbalance
-    else:
-        raise NotImplementedError()
-    return method_config + ' ' + dataset_config + ' --seed ' + seed.__str__()
-
-
-def experiment(method, dataset, seed, options, records, records_file, cache_file, output_path):
+def experiment(records, records_file, cache_file, output_path, **kwargs):
     done = False
     for record in records:
-        # if record['method'] == method and record['dataset'] == dataset and record['seed'] == seed:
-        if record['method'] == method and f"{record['dataset']}_{str(int(float(record['imb_ratio'])))}" == dataset and record['seed'] == seed:
+        same = True
+        for key, value in kwargs.items():
+            if record[key] != value:
+                same = False
+                break
+        if same:
             done = True
+            break
     
     if done:
         print('Have done!')
@@ -98,17 +43,15 @@ def experiment(method, dataset, seed, options, records, records_file, cache_file
         else:
             output = ' > '
 
-        # net = 'GCN'
-        # output_file =  os.path.join(output_path, f'{method}-{dataset}-{seed}-{net}.pt')
+        command = "python src/main.py"
+        for key, value in kwargs.items():
+            command += f' --{key} {value}'
+        command += options_more + output + cache_file
 
-        command = f"python src/main.py " + config_args(method=method, dataset=dataset, seed=seed) + " " + options + options_more + output + cache_file
-
-        print('\n')
+        print()
         print(command)
 
-        # begin_datetime = datetime.datetime.now()
         os.system(command)
-        # end_datetime = datetime.datetime.now()
 
         with open(cache_file) as f:
             info = f.read()
@@ -117,24 +60,6 @@ def experiment(method, dataset, seed, options, records, records_file, cache_file
                 record = json.loads(match.groups()[0])
                 print(record)
                 records.append(record)
-                
-            # Obsolete
-            # match = re.match('[\\S\\s]*acc: ([\\d\\.]*), bacc: ([\\d\\.]*), f1: ([\\d\\.]*), auc: ([\\d\\.]*)\n', info)
-            # if match is not None:
-            #     acc, bacc, f1, auc = tuple(map(lambda x: float(x), match.groups()))
-            #     record = dict()
-            #     record['begin_datetime'] = begin_datetime.__str__()
-            #     record['end_datetime'] = end_datetime.__str__()
-            #     record['time_erased'] = (end_datetime - begin_datetime).__str__()
-            #     record['method'] = method
-            #     record['dataset'] = dataset
-            #     record['seed'] = seed
-            #     record['acc'] = acc
-            #     record['bacc'] = bacc
-            #     record['f1'] = f1
-            #     record['auc'] = auc
-            #     print(record)
-            #     records.append(record)
 
         if not args.debug:
             os.system("rm " + cache_file)
@@ -144,7 +69,7 @@ def experiment(method, dataset, seed, options, records, records_file, cache_file
             json.dump(records, f, indent=4)
 
 
-def benchmark(name, methods, datasets, seeds):
+def benchmark(name, **config):
     suffix = ''
 
     if args.gpu:
@@ -170,26 +95,42 @@ def benchmark(name, methods, datasets, seeds):
             records = json.load(f)
     else:
         records = []
+        
+    try:
+        if os.path.exists(cache_file):
+            os.system("rm " + cache_file)
 
-    if os.path.exists(cache_file):
-        os.system("rm " + cache_file)
-
-    print(f'''
-    benchmark_{name}
-
-    methods = {methods}
-    datasets = {datasets}
-    seeds = {seeds}
-    ''')
-
-    for method in methods:
-        for dataset in datasets:
-            for seed in seeds:
-                experiment(method=method, dataset=dataset, seed=seed, options='', 
-                           records=records, records_file=records_file, cache_file=cache_file, output_path=output_path)
+        info = f'benchmark: {name}'
+        print('=' * len(info))
+        print(info)
+        print('-' * len(info))
+        
+        config_key_list = list(config.keys())
+        current_config_index = [0] * len(config)
+        
+        while True:
+            current_config = {config_key_list[i]: config[config_key_list[i]][current_config_index[i]] for i in range(len(config))}
+            for key, value in current_config.items():
+                print(f'{key} = {value}')
+            experiment(records=records, records_file=records_file, cache_file=cache_file, output_path=output_path,  **current_config)
+            ptr = len(records) - 1
+            while ptr >= 0:
+                current_config_index[ptr] += 1
+                if current_config_index[ptr] >= len(config[config_key_list[ptr]]):
+                    current_config_index[ptr] = 0
+                    ptr -= 1
+                else:
+                    break
+            if ptr == -1:
+                break
+            
+    except KeyboardInterrupt:
+        with open(records_file, 'w+') as f:
+            json.dump(records, f, indent=4)
 
 
 if __name__ == '__main__':
+    args = parse_args()
     name = args.name
     config_file = 'benchmark/' + name + '.json'
     with open('dictionary.json') as f_dictionary:
@@ -197,18 +138,27 @@ if __name__ == '__main__':
         if os.path.isfile(config_file):
             with open(config_file) as f:
                 config = json.load(f)  # A list of valid names
-                for config_option in ['methods', 'datasets', 'seeds']:
-                    if type(config[config_option]) != list:
-                        if config[config_option] in dictionary[config_option]:  # Use dictionary.json
-                            config[config_option] = dictionary[config_option][config[config_option]]
-                        elif config[config_option] in all_config[config_option]:  # Any valid name
-                            config[config_option] = [config[config_option]]
-                        elif config[config_option] == 'all':  # 'all'
-                            config[config_option] = all_config[config_option]
-                        # else:
-                        #     raise NotImplementedError
-                    elif len(config[config_option]) == 0:  # If it is an empty list, equivalent to 'all'
-                        config[config_option] = all_config[config_option]
-                benchmark(name=name, methods=config['methods'], datasets=config['datasets'], seeds=config['seeds'])
+                for key, value in config.items():
+                    if not isinstance(value, list):
+                        if key in dictionary and value in dictionary[key]:  # Use dictionary.json
+                            config[key] = dictionary[key][value]
+                        else:
+                            config[key] = [value]
+                    elif len(value) == 0:  # If it is an empty list, equivalent to 'all'
+                        config[key] = dictionary[key]["all"]
+                default = {
+                    'method': 'vanilla',
+                    'net': 'GCN',
+                    
+                    'dataset': 'Cora',
+                    'split': 'lt',
+                    'imb_ratio': 20,
+                    
+                    'seed': 100,
+                }
+                for key, value in default.items():
+                    if key not in config:
+                        config[key] = [value]
+                benchmark(name=name, **config)
         else:
             raise FileNotFoundError
